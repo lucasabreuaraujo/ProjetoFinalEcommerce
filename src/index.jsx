5242c035cf981cff1fbce1046374f692c94677fd
Lucas Abreu <lucas.abreuaraujo10@gmail.com>
2022-10-31 16:20:36 -0300

    configurando o netlify

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import ProductContexProvider from "./context/produtosContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,4 +15,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-serviceWorkerRegistration.register();
